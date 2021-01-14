630366c95591aee06963f03a26cadeafb6294bbe
Mihai Borobocea <138839+MihaiB@users.noreply.github.com>
2021-01-15 00:59:57 +0200

    bring back reading & writing of metadata from old/

--- file_ops.py
+++ file_ops.py
@@ -2,6 +2,7 @@ import hashlib
 import io
 import json
 import os, os.path
+import versionvectors
 
 
 META_FILE = '.vector-sync'
@@ -11,13 +12,13 @@ def check_file_hashes(h):
     """
     Raise an exception if ‘h’ is not file_hashes.
 
-    >>> check_file_hashes("bad")
+    >>> check_file_hashes('bad')
     Traceback (most recent call last):
     ValueError: file hashes is not dict
-    >>> check_file_hashes({3: ""})
+    >>> check_file_hashes({3: ''})
     Traceback (most recent call last):
     ValueError: file hashes key is not str
-    >>> check_file_hashes({"": {}})
+    >>> check_file_hashes({'': {}})
     Traceback (most recent call last):
     ValueError: file hashes value is not str
     """
@@ -82,3 +83,48 @@ def _hash_file_tree(tree_path, *, is_root):
 
     check_file_hashes(file_hashes)
     return file_hashes
+
+
+def check_meta_data(md):
+    """
+    Raise an exception if md is not meta data.
+
+    >>> check_meta_data(None)
+    Traceback (most recent call last):
+    ValueError: meta data is not dict
+    >>> check_meta_data({'bad': {}, 'keys': ''})
+    Traceback (most recent call last):
+    ValueError: invalid meta data keys
+    >>> check_meta_data({'id': 7, 'version_vector': {}, 'file_hashes': {}})
+    Traceback (most recent call last):
+    ValueError: meta data id is not str
+    >>> check_meta_data({'id': 'A', 'version_vector': {3}, 'file_hashes': {}})
+    Traceback (most recent call last):
+    ValueError: version vector is not dict
+    >>> check_meta_data({'id': 'A', 'version_vector': {}, 'file_hashes': '?'})
+    Traceback (most recent call last):
+    ValueError: file hashes is not dict
+
+    >>> check_meta_data({'id': 'A', 'version_vector': {}, 'file_hashes': {}})
+    """
+    if type(md) is not dict:
+        raise ValueError('meta data is not dict')
+    if set(md.keys()) != {'id', 'version_vector', 'file_hashes'}:
+        raise ValueError('invalid meta data keys')
+    if type(md['id']) is not str:
+        raise ValueError('meta data id is not str')
+    versionvectors.check(md['version_vector'])
+    check_file_hashes(md['file_hashes'])
+
+
+def write_meta_data(md, filepath):
+    check_meta_data(md)
+    with open(filepath, 'w', encoding='utf-8') as f:
+        json.dump(md, f, indent=2, sort_keys=True)
+
+
+def read_meta_data(filepath):
+    with open(filepath, encoding='utf-8') as f:
+        md = json.load(f)
+    check_meta_data(md)
+    return md
--- test_file_ops.py
+++ test_file_ops.py
@@ -144,3 +144,52 @@ class TestHashFileTree(unittest.TestCase):
                 'photos/summer/beach': hash_bytes(tree['photos']['summer']['beach']),
                 'photos/summer/hotel': hash_bytes(tree['photos']['summer']['hotel']),
             })
+
+
+class TestWriteMetaData(unittest.TestCase):
+
+    def test_error_for_dir(self):
+        with tempfile.TemporaryDirectory() as d:
+            md = {'id': 'A', 'version_vector': {}, 'file_hashes': {}}
+            with self.assertRaises(IsADirectoryError):
+                file_ops.write_meta_data(md, d)
+
+    def test_write_and_overwrite(self):
+        with tempfile.TemporaryDirectory() as d:
+            filepath = os.path.join(d, 'some-file')
+
+            md1 = {'id': 'A', 'version_vector': {'USB': 3}, 'file_hashes': {}}
+            file_ops.write_meta_data(md1, filepath)
+            with open(filepath, encoding='utf-8') as f:
+                self.assertEqual(f.read(), '''{
+  "file_hashes": {},
+  "id": "A",
+  "version_vector": {
+    "USB": 3
+  }
+}''')
+
+            md2 = {'id': 'B', 'version_vector': {}, 'file_hashes': {'a/b': 'h'}}
+            file_ops.write_meta_data(md2, filepath)
+            with open(filepath, encoding='utf-8') as f:
+                self.assertEqual(f.read(), '''{
+  "file_hashes": {
+    "a/b": "h"
+  },
+  "id": "B",
+  "version_vector": {}
+}''')
+
+
+class TestReadMetaData(unittest.TestCase):
+
+    def test_write_read(self):
+        def get_md():
+            return {
+                'id': 'Backup',
+                'file_hashes': {'path/to/file': 'the hash'},
+                'version_vector': {'Computer': 3, 'Remote': 8},
+            }
+        with tempfile.NamedTemporaryFile() as f:
+            file_ops.write_meta_data(get_md(), f.name)
+            self.assertEqual(file_ops.read_meta_data(f.name), get_md())
